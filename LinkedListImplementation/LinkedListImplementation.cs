1e0b5cc88565ca2bc7662f2d19e86b81cbf091ee
Shashko Valerii <shashko.valera2214@gmail.com>
2023-08-12 13:19:49 +0300

    Add bunch of mothods

--- LinkedListImplementation/LinkedListImplementation.cs
+++ LinkedListImplementation/LinkedListImplementation.cs
@@ -17,13 +17,13 @@ namespace LinkedListImplementation
 
         public MyLinkedList(IEnumerable<T> collection)
         {
-            head = new Node<T> (collection.First());
+            head = new Node<T>(collection.First());
             var current = head;
             tail = current;
 
             foreach (var item in collection.Skip(1))
             {
-                var newCurrent = new Node<T> (item);
+                var newCurrent = new Node<T>(item);
                 current.Next = newCurrent;
                 newCurrent.Prev = current;
                 current = newCurrent;
@@ -46,7 +46,7 @@ namespace LinkedListImplementation
 
                 current = current.Next;
             }
-           
+
             return min;
         }
 
@@ -67,6 +67,118 @@ namespace LinkedListImplementation
             }
 
             return max;
+        }
+
+        public void Add(T value)
+        {
+            var current = new Node<T>(value);
+            tail.Next = current;
+            current.Prev = tail;
+            tail = current;
+        }
+
+        public void AddStart(T value)
+        {
+            var current = new Node<T>(value);
+            head.Prev = current;
+            current.Next = head;
+            head = current;
+        }
+
+        public bool Contains(T value)
+        {
+            bool result = false;
+            var current = head;
+
+            while (current != null)
+            {
+                if (value.CompareTo(current.Data) == 0)
+                {
+                    result = true;
+                }
+
+                current = current.Next;
+            }
+
+            return result;
+        }
+
+        public void RemoveByValue(T value)
+        {
+            var current = head;
+
+            while (current != null)
+            {
+                if (value.CompareTo(current.Data) == 0)
+                {
+                    if (current.Prev == null)
+                    {
+                        head = current.Next;
+                        head.Prev = null;
+                    }
+                    else if (current.Next == null)
+                    {
+                        tail = current.Prev;
+                        tail.Next = null;
+                    }
+                    else
+                    {
+                        var newCurrent = current.Prev;
+                        newCurrent.Next = current.Next;
+                        newCurrent.Next.Prev = current.Prev;
+                        current = newCurrent;
+                    }
+
+                    break;
+                }
+
+                current = current.Next;
+            }
+        }
+
+        public void RemoveLastByValue(T value)
+        {
+            var current = tail;
+
+            while (current != null)
+            {
+                if (value.CompareTo(current.Data) == 0)
+                {
+                    if (current.Prev == null)
+                    {
+                        head = current.Next;
+                        head.Prev = null;
+                    }
+                    else if (current.Next == null)
+                    {
+                        tail = current.Prev;
+                        tail.Next = null;
+                    }
+                    else
+                    {
+                        var newCurrent = current.Prev;
+                        newCurrent.Next = current.Next;
+                        newCurrent.Next.Prev = current.Prev;
+                        current = newCurrent;
+                    }
+
+                    break;
+                }
+
+                current = current.Prev;
+            }
+        }
+
+        public void RemoveFirst()
+        {
+            head = head.Next;
+            head.Prev = null;
+        }
+
+        public void RemoveLast()
+        {
+            tail = tail.Prev;
+            tail.Next = null;
         }
 
         public void PrintList()
--- LinkedListImplementation/Program.cs
+++ LinkedListImplementation/Program.cs
@@ -4,9 +4,14 @@
     {
         public static void Main(string[] args)
         {
-            int[] array = { 1, 2, 3, 4, 5, 0, -1};
+            int[] array = { 1, 2, 3, 4, 2, 5};
 
             MyLinkedList<int> list = new MyLinkedList<int>(array);
+            list.PrintList();
+            Console.WriteLine();
+            list.RemoveLast();
+            Console.WriteLine();
+            list.PrintList();
         }
     }
 }
